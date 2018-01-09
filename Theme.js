dd2a1d9cc023094e95a59329124dc4c668a658c4
Leonid <leonid-voevodov@mail.ru>
2018-01-09 08:12:27 +0800

    ChangeTheme.

--- Theme.js
+++ Theme.js
@@ -1,8 +1,16 @@
-function changeTheme(selectBackColor, selectColor="#dbdbdb") {
+function changeTheme(selectBackColor='#212121', selectColor="#dbdbdb") {
   var elements = document.querySelectorAll(".my-style");
   for (var i = 0; i < elements.length; i++) {
-	elements[i].style.backgroundColor=selectBackColor;
-	elements[i].style.color=selectColor;
+    elements[i].style.backgroundColor=selectBackColor;
+    elements[i].style.color=selectColor;
   }	
+
+  elements = document.querySelectorAll("a");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.color=selectColor;
+  }	
+
+  /*elements = document.getElementsByTagName('footer');
+  elements[0].style.color='green';*/
 }
 
